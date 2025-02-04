fde1fc41189247ebc755ee4a915eb2492f821e03
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2025-02-04 12:32:40 -0500

    sdfsdfs

--- app/README.md
+++ app/README.md
@@ -1,4 +1,4 @@
-# vite-project
+# app
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ components/App.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>PRODUCTs</h1>
+</template>
+
+<script>
+export default{
+ name: "App",
+
+ components: {}
+};
+</script>
+
+
+<style>
+</style>
